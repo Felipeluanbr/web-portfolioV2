dbe25eae523265e260b1e6c7c3e919d4b3a4c6f7
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-07-02 19:30:46 -0300

    Adicionando a tipografia

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logos/Logo.svg";
 
+
 function Navbar() {
   return (
-    <nav>
+    <nav className="font-handwriting">
       <div>
-        <div className="bg-red-500">
+        <div className="bg-light-red">
             <img src={Logo} alt="Logo" />
+            <h1>About</h1>
+            <h1>Sobre</h1>
         </div>
         <div className=""></div>
       </div>
